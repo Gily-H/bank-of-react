48e0dd20086357240a83e8b26ebc9b3c9de27295
Gilman Huang <67421795+Gily-H@users.noreply.github.com>
2022-01-22 12:58:00 -0500

    added home, userprofile, accountbalance pages

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,19 @@
 import React from "react";
+import AccountBalance from "./AccountBalance";
+import { Link } from "react-router-dom";
 
-const Home = () => {
+const Home = (props) => {
   return (
-    <div className="home">
-      <h2>Home</h2>
+    <div>
+      <img
+        src="https://letstalkpayments.com/wp-content/uploads/2016/04/Bank.png"
+        alt="bank"
+      />
+      <h1>Bank of React</h1>
+
+      <Link to="/userProfile">User Profile</Link>
+
+      <AccountBalance accountBalance={props.accountBalance} />
     </div>
   );
 };
