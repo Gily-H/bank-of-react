fc1720b8c508671a473ca1b5fde2a87d55bfd123
gilman huang <gilman.huang213@gmail.com>
2022-01-22 20:07:34 -0500

    added transaction functionality

--- src/App.js
+++ src/App.js
@@ -1,48 +1,84 @@
 import React, { useEffect } from "react";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
-import Debit from "./components/Debit"
-import Credit from "./components/Credit"
+import Debit from "./components/Debit";
+import Credit from "./components/Credit";
 import { Route, Routes } from "react-router-dom";
-import axios from 'axios'
+import axios from "axios";
 
 const App = () => {
   const [transactions, setTrans] = React.useState({
-    debit:[],
-    credit: []
-  })
-  const [balance, setBalance] = React.useState(0);
-  const[user, setUser] = React.useState(
-    {
-      userName:"",
-      memberSince:""
-    }
-  )
-  
-  useEffect(()=>{
-    const fetchTransactions = async () =>{
+    debit: [],
+    credit: [],
+  });
+  const [balance, setBalance] = React.useState(10234.23);
+  const [user, setUser] = React.useState({
+    userName: "",
+    memberSince: "",
+  });
+
+  useEffect(() => {
+    const fetchTransactions = async () => {
       const debit = await axios.get("https://moj-api.herokuapp.com/debits");
       const credit = await axios.get("https://moj-api.herokuapp.com/credits");
       setTrans({
         debit: debit.data,
-        credit: credit.data
-      })
-    }
+        credit: credit.data,
+      });
+    };
 
-    fetchTransactions()
-  }, [])
+    fetchTransactions();
+  }, []);
+
+  const chargeBalance = (transaction) => {
+    setBalance(
+      (prevBalance) => prevBalance - Number.parseFloat(transaction.amount).toFixed(2)
+    );
+  };
+
+  // add debit transaction to list
+  const chargeDebit = (transaction) => {
+    setTrans((prevTransactions) => ({
+      ...prevTransactions,
+      debit: [...prevTransactions.debit, transaction],
+    }));
+    chargeBalance(transaction); // update balance
+  };
+
+  // add credit transaction to list
+  const chargeCredit = (transaction) => {
+    setTrans((prevTransactions) => ({
+      ...prevTransactions,
+      credit: [...prevTransactions.credit, transaction],
+    }));
+    chargeBalance(transaction); // update balance
+  };
 
   return (
     <div className="App">
       <Navbar />
       <Routes>
-        <Route path={"/"} element={<Home/>}/>
-        <Route path={"/Debit"} element={<Debit
-          debit = {transactions.debit}
-        />}/>
-        <Route path={"/Credit"} element={<Credit
-          credit = {transactions.credit}
-        />}/>
+        <Route path={"/"} element={<Home />} />
+        <Route
+          path={"/Debit"}
+          element={
+            <Debit
+              debit={transactions.debit}
+              chargeDebit={chargeDebit}
+              balance={balance}
+            />
+          }
+        />
+        <Route
+          path={"/Credit"}
+          element={
+            <Credit
+              credit={transactions.credit}
+              chargeCredit={chargeCredit}
+              balance={balance}
+            />
+          }
+        />
       </Routes>
     </div>
   );
--- src/components/Debit.js
+++ src/components/Debit.js
@@ -1,35 +1,77 @@
-import React from 'react'
-import '../styles/transactions.css'
-import AccountBalance from './AccountBalance';
+import React, { useState } from "react";
+import "../styles/transactions.css";
+import AccountBalance from "./AccountBalance";
+import { nanoid } from "nanoid";
 
-const Debit = (props) =>{
+const Debit = (props) => {
+  const [formValues, setFormValues] = useState({
+    id: nanoid(),
+    description: "",
+    amount: "",
+    date: "",
+  });
 
-    function dateFix(date){
-        return date.slice(0, 10);
-    }
+  // cuts date to show YYYY-MM-DD
+  function dateFix(date) {
+    return date.slice(0, 10);
+  }
 
-    const ren = props.debit.map(data => (
-        <div className='transaction'>
-            <p><strong>{data.description}</strong></p>
-            <p>{`Spent: $${data.amount}`}</p>
-            <p>{"Purchased on: "+ dateFix(data.date)}</p> 
-        </div>
-        ))
-    
-        return(
-            <div className='transactionPage'>
-                 <div className='balance'>
-                    <AccountBalance/>
-                </div>
+  const ren = props.debit.map((data) => (
+    <div key={data.id} className="transaction">
+      <p>
+        <strong>{data.description}</strong>
+      </p>
+      <p>{`Spent: $${data.amount}`}</p>
+      <p>{"Purchased on: " + dateFix(data.date)}</p>
+    </div>
+  ));
 
-                <div>
-                    <h1 className='heading'>Debit transactions</h1>
-                    {ren}
-                </div>
-                
-               
-            </div>
-        )
-}
+  const onChange = (event) => {
+    setFormValues((prevFormValues) => ({
+      ...prevFormValues,
+      [event.target.name]: event.target.value,
+    }));
+  };
 
-export default Debit
+  const formSubmit = (event) => {
+    event.preventDefault();
+    props.chargeDebit(formValues);
+
+    setFormValues({
+      id: nanoid(), // new transaction id
+      description: "",
+      amount: "",
+      date: "",
+    });
+  };
+
+  return (
+    <div className="transactionPage">
+      <div className="balance">
+        <AccountBalance balance={props.balance} />
+      </div>
+
+      <form className="transaction-form" onSubmit={formSubmit}>
+        <label>Description</label>
+        <input
+          name="description"
+          type="text"
+          value={formValues.description}
+          onChange={onChange}
+        />
+        <label>Transaction Amount</label>
+        <input name="amount" type="text" value={formValues.amount} onChange={onChange} />
+        <label>Transaction Date</label>
+        <input name="date" type="text" value={formValues.date} onChange={onChange} />
+        <button>Add Transaction</button>
+      </form>
+
+      <div>
+        <h1 className="heading">Debit transactions</h1>
+        {ren}
+      </div>
+    </div>
+  );
+};
+
+export default Debit;
